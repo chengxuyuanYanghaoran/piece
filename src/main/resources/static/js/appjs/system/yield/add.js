4ff6de50142980e890d44fce637de49f9ed7b943
chengxuyuanYanghaoran <1812582971@qq.com>
2019-12-17 09:59:16 +0800

    2019年12月17日 统计表单页面添加时校验，校验失败前端进行拦截 杨浩然

--- src/main/resources/static/js/appjs/system/yield/add.js
+++ src/main/resources/static/js/appjs/system/yield/add.js
@@ -1,16 +1,27 @@
-// $().ready(function() {
-// 	// validateRule();
-// });
+$().ready(function() {
+    var mode=parent.parent.parent.$("input[name='state']:checked").val();//获取计价的方式;
+	if (mode==0){ //进行产品校验
+        validateRule();
+	}else {
+        validateRule2();
+	}
+});
 
-// $.validator.setDefaults({
-// 	submitHandler : function() {
-// 	    alert(1);
-// 		save();
-// 	}
-// });
+/*
+* 校验未通过时拦截submit请求
+* */
+$.validator.setDefaults({
+	submitHandler : function() {
+		save();//调用保存方法
+	}
+});
+/*
+* 保存数据的方法
+* */
 function save() {
     var mode=parent.parent.parent.$("input[name='state']:checked").val();//获取计价的方式;
     $("#mode").val(mode);//修改隐藏表单域的值
+
 	$.ajax({
 		cache : true,
 		type : "POST",
@@ -37,107 +48,96 @@ function save() {
 	});
 
 }
-// function validateRule() {
-// 	var icon = "<i class='fa fa-times-circle'></i> ";
-// 	$("#signupForm").validate({
-// 		rules : {
-//             yieldCode: {
-//                 required: true,
-//                 remote: {
-//                     url: "/system/yield/validateByCard",     //后台处理程序
-//                     type: "post",               //数据发送方式
-//                     dataType: "json",           //接受数据格式
-//                     data: {                     //要传递的数据
-//                         yieldCode: function () {
-//                             return $("#yieldCode").val()
-//                         }
-//                     }
-//                 }
-//             },
-//             peopleCode: {
-//                 required: true,
-//                 remote: {
-//                     url: "/system/yield/validateByCard",     //后台处理程序
-//                     type: "post",               //数据发送方式
-//                     dataType: "json",           //接受数据格式
-//                     data: {                     //要传递的数据
-//                         peopleCode: function () {
-//                             return $("#peopleCode").val()
-//                         }
-//                     }
-//                 }
-//         },
-//             proCode: {
-//                 required: true,
-//                 remote: {
-//                     url: "/system/yield/validateByCard",     //后台处理程序
-//                     type: "post",               //数据发送方式
-//                     dataType: "json",           //接受数据格式
-//                     data: {                     //要传递的数据
-//                         proCode: function () {
-//                             return $("#proCode").val()
-//                         }
-//                     }
-//                 }
-//         },
-//         productCode: {
-//                 required: true,
-//                 remote: {
-//                     url: "/system/yield/validateByCard",     //后台处理程序
-//                     type: "post",               //数据发送方式
-//                     dataType: "json",           //接受数据格式
-//                     data: {                     //要传递的数据
-//                         productCode: function () {
-//                             return $("#productCode").val()
-//                         }
-//                     }
-//                 }
-//         },
-//             dateMark: {
-//                 required: true
-//             },
-//             name: {
-//                 required: true
-//             },
-//             proName: {
-//                 required: true
-//             },
-//             productName: {
-//                 required: true
-//             }
-//         },
-//
-//
-//         messages : {
-//             yieldCode : {
-// 				required : icon + "请输入单据编号",
-//                 remote:icon+"单据编号已存在"
-//              },
-//             peopleCode : {
-//                 required : icon + "请输入人员编码",
-//                 remote:icon+"不能输入重复的人员编码"
-//             },
-//             proCode : {
-//             required : icon + "请输入工序编码",
-//                 remote:icon+"不能输入重复的工序编码"
-//         },
-//             productCode : {
-//             required : icon + "请输入产品编码",
-//                 remote:icon+"不能输入重复的产品编码"
-//         },
-//             dateMark : {
-//                 required : icon + "请输入日期",
-//             },
-//             name : {
-//                 required : icon + "请输入姓名",
-//             },
-//             proName : {
-//                 required : icon + "请输入工序名称",
-//             },
-//             productName : {
-//                 required : icon + "请输入产品名称",
-//             }
-//
-// 		}
-// 	})
-// }
+
+/*
+* 产品校验
+* */
+function validateRule() {
+	var icon = "<i class='fa fa-times-circle'></i> ";
+	$("#signupForm").validate({
+		rules : {
+            // yieldCode: {
+            //     required: true,
+            //     remote: {
+            //         url: "/system/yield/validateByCard",     //后台处理程序
+            //         type: "post",               //数据发送方式
+            //         dataType: "json",           //接受数据格式
+            //         data: {                     //要传递的数据
+            //             yieldCode: function () {
+            //                 return $("#yieldCode").val()
+            //             }
+            //         }
+            //     }
+            // },
+            dateMark: {
+                required: true
+            },
+            name: {
+                required: true
+            },
+            productName: {
+                required: true
+            },
+            harvest:{
+                required: true
+			}
+        },
+
+        messages : {
+        //     yieldCode : {
+			// 	required : icon + "请输入单据编号",
+        //         remote:icon+"单据编号已存在"
+        //      },
+            dateMark : {
+                required : icon + "请输入日期",
+            },
+            name : {
+                required : icon + "请输入姓名",
+            },
+            productName : {
+                required : icon + "请输入产品名称",
+            },
+            harvest : {
+                required : icon + "请输入数量",
+			}
+		}
+	})
+}
+
+/*
+* 工序校验
+* */
+function validateRule2() {
+    var icon = "<i class='fa fa-times-circle'></i> ";
+    $("#signupForm").validate({
+        rules : {
+            dateMark: {
+                required: true
+            },
+            name: {
+                required: true
+            },
+            proName: {
+                required: true
+            },
+            harvest:{
+                required: true
+            }
+        },
+
+        messages : {
+            dateMark : {
+                required : icon + "请输入日期",
+            },
+            name : {
+                required : icon + "请输入姓名",
+            },
+            proName : {
+                required : icon + "请输入工序名称",
+            },
+            harvest : {
+                required : icon + "请输入数量",
+            }
+        }
+    })
+}
